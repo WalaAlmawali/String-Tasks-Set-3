da092c0a1e3b6d89c68bebd504973adc778c0134
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-21 16:14:07 +0400

    Loop through each index in the string + Use if statement to Check for is + update isCount

--- src/EqualIsNot.java
+++ src/EqualIsNot.java
@@ -11,6 +11,11 @@ public class EqualIsNot {
         int isCount = 0;
         int notCount = 0;
 
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.substring(i, i + 2).equals("is")) {
+                isCount++;
+            }
 
 
 
