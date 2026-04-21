7dbc42ae572bdfe64f479941f6fbcb163786fb9e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-21 15:22:49 +0400

    use contains() method to check substring is in the original string or not+ add for loop to go through each index in the original string + Use conteinu to skip substring part from the original string

--- src/WithoutString.java
+++ src/WithoutString.java
@@ -9,10 +9,23 @@ public class WithoutString {
 
     public static String withoutString(String str, String str1) {
 
+        String result = "" ;
+        if(str.contains(str1)){
+           int startIndex= str.indexOf(str1);
 
 
+           for(int i =0 ; i< str.length();i++){
+               if(i==startIndex || i<= str1.length() +1 && i>=startIndex){
+                   continue;
+               } else{
+                   result+= str.charAt(i);
+               }
+
+           }
 
 
+        }
+        return result;
     }
 
     }
