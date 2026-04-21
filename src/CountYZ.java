1bc6282f202d0473556e501efae9f726f51c1388
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-21 14:42:16 +0400

    Declear Integer variable named count + Loop through each character in the string

--- src/CountYZ.java
+++ src/CountYZ.java
@@ -9,6 +9,14 @@ public class CountYZ {
     public static int countYZ(String str ){
 
         str= str.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+
+
+
+        }
 
 
 
