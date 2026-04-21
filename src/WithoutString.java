98742401443a9f1b0700b23b62d00f34f93abfc5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-21 16:05:05 +0400

    Update withoutString function

--- src/WithoutString.java
+++ src/WithoutString.java
@@ -1,11 +1,11 @@
-import java.util.*;
+
 
 public class WithoutString {
     public static void main(String[] args) {
 
 
         String str = "Hello there";
-        String str1 = "x";
+        String str1 = "e";
 
 
         System.out.println(withoutString(str,str1));
@@ -13,16 +13,16 @@ public class WithoutString {
     }
 
     public static String withoutString(String str, String str1) {
-        Set<Character> set = new HashSet<>();
 
         String result = "" ;
         if(str.contains(str1)){
             if(str1.length()>1){
-           int startIndex= str.indexOf(str1);
 
+           int startIndex= str.indexOf(str1);
+           int lastIndex = startIndex + str1.length() -1;
 
            for(int i =0 ; i< str.length();i++){
-               if(i==startIndex || i<= str1.length()+1 && i>=startIndex){
+               if(i==startIndex || i>startIndex && i<= lastIndex ){
                    continue;
                } else{
                    result+= str.charAt(i);
@@ -31,11 +31,9 @@ public class WithoutString {
            }
 
         } else if(str1.length()<=1){
-
-                set.add(str1.charAt(0));
                 for(int i =0 ; i< str.length();i++){
 
-                    if(set.contains(str.charAt(i))){
+                    if(str.charAt(i) == str1.charAt(0)){
                         continue;
                     }else{
                         result+= str.charAt(i);
