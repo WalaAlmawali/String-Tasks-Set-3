239dec35a7e9261126250ac89aa6c782dfb31dc9
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 14:53:18 +0400

    Create a string variable named newStr +Inside the loop add an if condetions +create object of the class and Call notReplace function in the main method

--- src/NotReplace.java
+++ src/NotReplace.java
@@ -1,21 +1,42 @@
 public class NotReplace {
     public static void main(String[] args) {
 
-
-
+        NotReplace ex = new NotReplace();
+        System.out.print(ex.notReplace("This is right"));
 
     }
     public String notReplace(String str) {
 
+
+        String newStr ="";
+
         for (int i = 0; i < str.length(); i++) {
 
 
+            if((i == 0 && str.substring(0,i+2).equals("is")) && !(Character.isLetter(str.charAt(2)))){
+
+                newStr += "is not";
+                i++;
+                continue;
+           }
+
+
+
+            if(i > 0 && i< str.length()-2 &&(str.substring(i,i+2).equals("is")) && !(Character.isLetter(str.charAt(i+2))) && !(Character.isLetter(str.charAt(i-1)))){
+               newStr += "is not";
+               i++;
+               continue;
+            }
+
+            if(i== str.length()-2 && str.substring(str.length()-2).equals("is") && !(Character.isLetter(str.charAt(str.length()-3)))){
+                newStr += "is not";
+                break;
+            }
+                newStr+= str.charAt(i);
+
 
         }
-
-
-
-
+           return newStr;
         }
 
     }
