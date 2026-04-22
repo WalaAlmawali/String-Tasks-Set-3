df1144746ad784146668cd9b00791556c04534bb
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 09:15:03 +0400

    Update if part in the function + create object of the class + Call sumDigits function in the main method

--- src/SumDigits.java
+++ src/SumDigits.java
@@ -1,7 +1,8 @@
 public class SumDigits {
     public static void main(String[] args) {
 
-
+        SumDigits ex = new SumDigits();
+        System.out.println(ex.sumDigits("aa1bc2d3"));
 
 
     }
@@ -12,7 +13,7 @@ public class SumDigits {
         for (int i = 0; i < str.length(); i++) {
 
             if (Character.isDigit(str.charAt(i))) {
-                sum += str.charAt(i);
+                sum += str.charAt(i) - '0';
             }
 
         }
