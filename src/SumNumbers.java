bd7f26b8b23160df29adb6e25cee6fcb3910947c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 11:43:46 +0400

    Add if-else condetion and use Character.isDigit() to check if th string character is digit or not + update sum value + Add return part

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -11,12 +11,20 @@ public class SumNumbers {
         int Number = 0;
 
         for (int i = 0; i < str.length(); i++) {
-
             char ch = str.charAt(i);
 
+            if (Character.isDigit(ch)) {
+                Number = Number * 10 + (ch - '0');
+
+            } else {
+                sum += Number;
+                Number = 0;
+            }
 
         }
+        sum += Number;
 
+        return sum;
 
     }
 
