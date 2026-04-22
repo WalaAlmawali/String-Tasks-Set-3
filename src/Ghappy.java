47557a4ab966f0dee497aa288dbf51ecefae8a5d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 08:33:31 +0400

    update if part in gHappy function + Inside the main method create object of the class and call gHappy function

--- src/Ghappy.java
+++ src/Ghappy.java
@@ -2,23 +2,23 @@ public class Ghappy {
     public static void main(String[] args) {
 
 
+        Ghappy ex = new Ghappy();
+        System.out.println(ex.gHappy("xxggyygxx"));
 
 
     }
-    public boolean gHappy(String str){
 
-        boolean happy = false;
+    public boolean gHappy(String str) {
+        boolean happy = true;
 
-        for(int i =0 ; i<str.length()- 1;i++){
-
-
-            if(str.charAt(i) == 'g' && str.charAt(i-1)=='g'||str.charAt(i+1)=='g'){
-                happy = true;
-
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == 'g') {
+                if (!(i > 0 && str.charAt(i - 1) == 'g') && !(i < str.length() - 1 && str.charAt(i + 1) == 'g')) {
+                    happy= false;
+                }
             }
 
         }
         return happy;
     }
-
-    }
+}
