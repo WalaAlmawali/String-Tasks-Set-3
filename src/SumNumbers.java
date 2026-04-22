1506687ea94f35c8a42fd9f12168f1ce0922ab28
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 11:32:00 +0400

    Declear two Integer variable named sum and Number + Declear char variable named ch

--- src/SumNumbers.java
+++ src/SumNumbers.java
@@ -7,9 +7,12 @@ public class SumNumbers {
 
     public int sumNumbers(String str) {
 
+        int sum = 0;
+        int Number = 0;
 
         for (int i = 0; i < str.length(); i++) {
 
+            char ch = str.charAt(i);
 
 
         }
