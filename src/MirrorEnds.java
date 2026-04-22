d1beae62a973ccd86f26c442c1817faf139518d7
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 10:22:54 +0400

    Add if condetion

--- src/MirrorEnds.java
+++ src/MirrorEnds.java
@@ -9,10 +9,11 @@ public class MirrorEnds {
     public String mirrorEnds(String str) {
 
         for(int i = 0; i< str.length();i++){
+            if(str.charAt(i)!= str.charAt(str.length()- 1 - i)){
+                return str.substring(0,i);
+            }
 
         }
-
-
+        return "";
     }
-
-    }
+ }
