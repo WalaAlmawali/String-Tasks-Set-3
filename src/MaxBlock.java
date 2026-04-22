4be5750dbd5a0abc91ecd42f74ebe5deedd868f3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 10:55:53 +0400

    Declear two Integer variable named max and count + Add an if else condetion

--- src/MaxBlock.java
+++ src/MaxBlock.java
@@ -5,11 +5,22 @@ public class MaxBlock {
 
     }
     public int maxBlock(String str){
+        int max = 0;
+        int count = 0;
 
        for(int i =0 ; i< str.length();i++){
 
+           if(str.charAt(i)== str.charAt(i+1)){
+               count++;
+           }else {
+               if(count>max){
+                   max = count;
+               }
+           }
+
 
        }
+       return max;
     }
 
     }
