33774388c9c504b8a392fd83690dd8eda99ba5d4
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-22 11:13:56 +0400

    Update if condetion part + create object of the class + Call maxBlock function in tha main method

--- src/MaxBlock.java
+++ src/MaxBlock.java
@@ -1,26 +1,30 @@
 public class MaxBlock {
     public static void main(String[] args) {
 
+        MaxBlock ex = new MaxBlock();
+        System.out.print(ex.maxBlock("abbCCCddBBBxx"));
 
 
     }
     public int maxBlock(String str){
-        int max = 0;
-        int count = 0;
+        int max = 1;
+        int count = 1;
 
-       for(int i =0 ; i< str.length();i++){
+       for(int i =0 ; i< str.length()-1;i++){
 
            if(str.charAt(i)== str.charAt(i+1)){
                count++;
-           }else {
-               if(count>max){
+           }else{
+               count = 1;
+           }
+               if(count > max){
                    max = count;
                }
            }
 
-
+        return max;
        }
-       return max;
-    }
 
     }
+
+
